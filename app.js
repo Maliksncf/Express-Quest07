2a53bb811fb171441428134dda668207f00d9855
Maliksncf <abdelmalik-el-kaddous.bougherara@sncf.fr>
2022-09-13 00:07:54 +0200

    second one

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+const {
+  getUserById, 
+  updateUser, 
+  deleteUser, 
+  getUsers, 
+  postUser 
+} = require("./userHandlers");
+
+const {
+  getMovieById,
+  getMovies,
+  updateMovie,
+  deleteMovie,
+  postMovie
+} = require("./movieHandlers");
+
+const { validateUser } = require("./validators.js");
+const { validateMovie } = require("./validators.js");
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
@@ -18,21 +37,21 @@ const { hashPassword } = require("./auth.js");
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
+app.get("/api/movies", getMovies);
+app.get("/api/movies/:id", getMovieById);
+app.post("/api/movies", validateMovie, postMovie);
+app.put("/api/movies/:id", updateMovie);
+app.delete("/api/movies/:id", deleteMovie);
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
+app.get("/api/users", getUsers);
+app.get("/api/users/:id", getUserById);
+app.post("/api/users", hashPassword, postUser);
+app.put("/api/users/:id", validateUser, updateUser);
+app.delete("/api/users/:id", deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
