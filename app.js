3ecf40e2e52acac14887347a98d93e7250d4f3e7
Maliksncf <abdelmalik-el-kaddous.bougherara@sncf.fr>
2022-09-13 09:54:19 +0200

    authentification jwt

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const {
   updateUser, 
   deleteUser, 
   getUsers, 
-  postUser 
+  postUser,
+  getUserByEmailWithPasswordAndPassToNext 
 } = require("./userHandlers");
 
 const {
@@ -31,13 +32,27 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 
 
 
 app.get("/", welcome);
 
+// the public routes
 
+app.post(
+  "/api/login",
+  getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+); // /!\ login should be a public route
+
+// then the routes to protect
+
+
+
+
+
+// Public ROUTES
 
 app.get("/api/movies", getMovies);
 app.get("/api/movies/:id", getMovieById);
@@ -45,13 +60,25 @@ app.post("/api/movies", validateMovie, postMovie);
 app.put("/api/movies/:id", updateMovie);
 app.delete("/api/movies/:id", deleteMovie);
 
+// Protected ROUTES
 
+app.post("/api/movies", verifyToken, postMovie);
+app.put("/api/movies/:id", verifyToken, updateMovie);
+app.delete("/api/movies/:id", verifyToken, deleteMovie);
+
+// Public ROUTES
 
 app.get("/api/users", getUsers);
 app.get("/api/users/:id", getUserById);
 app.post("/api/users", hashPassword, postUser);
 app.put("/api/users/:id", validateUser, updateUser);
 app.delete("/api/users/:id", deleteUser);
+
+// Protected ROUTES
+
+app.post("/api/users", verifyToken, postUser);
+app.put("/api/users/:id", verifyToken, updateUser);
+app.delete("/api/users/:id", verifyToken, deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
